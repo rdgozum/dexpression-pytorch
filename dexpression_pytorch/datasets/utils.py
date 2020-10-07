3c9e8c4552022677cb7986296fdb6658fb6ec7e7
rdgozum <ryanpaul.gozum@gmail.com>
2020-10-08 03:20:27 +0800

    Convert numpy arrays into torch tensors

--- dexpression_pytorch/datasets/utils.py
+++ dexpression_pytorch/datasets/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from sklearn.model_selection import KFold
 
 
@@ -6,3 +7,21 @@ def kfold(x, y, splits=5, shuffle=True):
     kfold = KFold(n_splits=splits, shuffle=shuffle)
     for train, test in kfold.split(x, y):
         yield train, test
+
+
+def convert_to_torch(x_train, y_train, x_test, y_test):
+    # converting training images into torch format
+    x_train = torch.from_numpy(x_train)
+
+    # converting the label into torch format
+    y_train = y_train.astype(int)
+    y_train = torch.from_numpy(y_train)
+
+    # converting test images into torch format
+    x_test = torch.from_numpy(x_test)
+
+    # converting the label into torch format
+    y_test = y_test.astype(int)
+    y_test = torch.from_numpy(y_test)
+
+    return x_train, y_train, x_test, y_test
--- dexpression_pytorch/main.py
+++ dexpression_pytorch/main.py
@@ -4,8 +4,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
-from dexpression_pytorch.datasets import dataset
-from dexpression_pytorch.datasets import utils
+from dexpression_pytorch.datasets import dataset, utils
 
 
 def run():
@@ -13,10 +12,12 @@ def run():
     folds = utils.kfold(x, y)
 
     for train, test in folds:
-        print(x[train].shape)
-        print(y[train].shape)
-        print(x[test].shape)
-        print(y[test].shape)
+        x_train, y_train, x_test, y_test = utils.convert_to_torch(
+            x[train], y[train], x[test], y[test]
+        )
+
+        # training.run(x_train, y_train)
+        # testing.run(x_test, y_test)
 
 
 if __name__ == "__main__":

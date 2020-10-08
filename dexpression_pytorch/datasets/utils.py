88a97878f43849ebd216584ca704af3cdca89731
rdgozum <ryanpaul.gozum@gmail.com>
2020-10-09 03:18:49 +0800

    Replace comments in function utils.convert_to_torch

--- dexpression_pytorch/datasets/utils.py
+++ dexpression_pytorch/datasets/utils.py
@@ -8,6 +8,7 @@ from sklearn.utils import shuffle as s
 def kfold(x, y, splits=10, shuffle=True):
     x, y = s(x, y)
     kfold = KFold(n_splits=splits, shuffle=True)
+
     for train, test in kfold.split(x, y):
         x_train, y_train = x[train], y[train]
         x_test, y_test = x[test], y[test]
@@ -16,19 +17,19 @@ def kfold(x, y, splits=10, shuffle=True):
 
 
 def convert_to_torch(x_train, y_train, x_test, y_test):
-    # converting training images into torch format
+    # converting training images into torch tensor
     x_train = torch.from_numpy(x_train)
     x_train = x_train.type(torch.FloatTensor)
 
-    # converting the label into torch format
+    # converting the label into torch tensor
     y_train = y_train.astype(int)
     y_train = torch.from_numpy(y_train)
 
-    # converting test images into torch format
+    # converting test images into torch tensor
     x_test = torch.from_numpy(x_test)
     x_test = x_test.type(torch.FloatTensor)
 
-    # converting the label into torch format
+    # converting the label into torch tensor
     y_test = y_test.astype(int)
     y_test = torch.from_numpy(y_test)
 
